224b37fdd0303affc821105f4bd8cb311fb98546
Jihyeon Kim <develop3357@gmail.com>
2022-12-13 15:45:35 +0900

    Setup styled-components

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import styled, { createGlobalStyle, DefaultTheme } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:black;
+  line-height: 1.2;
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}
+`;
+
+export const theme: DefaultTheme = {
+  bgColor: "#3f8cf2",
+  boardColor: "#dadfe9",
+  cardColor: "white",
+};
