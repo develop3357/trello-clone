dfdfcb25fa4a3ea2279ae08ee51816ba16304565
Jihyeon Kim <develop3357@gmail.com>
2022-12-14 13:35:15 +0900

    Add persistance

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,19 @@
-import { atom } from "recoil";
+import { atom, AtomEffect } from "recoil";
+
+const localStorageEffect =
+  (key: string): AtomEffect<ITodoState> =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
 
 export interface ITodo {
   id: number;
@@ -11,5 +26,10 @@ interface ITodoState {
 
 export const toDoState = atom<ITodoState>({
   key: "toDo",
-  default: { "To Do": [], Doing: [], Done: [] },
+  default: {
+    "To Do": [],
+    Doing: [],
+    Done: [],
+  },
+  effects: [localStorageEffect("toDo")],
 });
